abfae3e8d5646b5e5a8b06808ee95d78ca9b0050
Aleksandr <ac5295@student.jamk.fi>
2025-04-04 22:27:35 +0300

    feat(general): create project, add dependencies

---
+++ src/main/java/com/example/springbootteamspirit/SpringBootTeamSpiritApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootteamspirit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTeamSpiritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTeamSpiritApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootteamspirit/SpringBootTeamSpiritApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootteamspirit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTeamSpiritApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
